fe27c59216e666437d9df4b1f5db47f1dc14648e
Joao Lucas <jldecristo@gmail.com>
2025-07-27 19:19:28 -0300

    construtores com sobrecarga do Client

--- src/entities/Client.java
+++ src/entities/Client.java
@@ -8,4 +8,13 @@ public class Client {
     private String email;
     private LocalDate birthDate;
 
+    public Client() {
+    }
+
+    public Client(String name, String email, LocalDate birthDate) {
+        this.name = name;
+        this.email = email;
+        this.birthDate = birthDate;
+    }
+
 }
