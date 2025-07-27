44ab22898a1d0e466289404eea0973191009af2a
Joao Lucas <jldecristo@gmail.com>
2025-07-27 19:21:45 -0300

    setters do Client

--- src/entities/Client.java
+++ src/entities/Client.java
@@ -29,4 +29,16 @@ public class Client {
         return birthDate;
     }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
 }
