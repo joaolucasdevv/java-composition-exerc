0c2cce51f171745c5613a315473693c714c5ed1b
Joao Lucas <jldecristo@gmail.com>
2025-07-27 19:25:34 -0300

    atributos da classe Order

--- src/entities/Order.java
+++ src/entities/Order.java
@@ -1,4 +1,17 @@
 package entities;
 
+import entities.enums.OrderStatus;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Order {
+
+    private LocalDate moment;
+    private OrderStatus status;
+
+    private Client client;
+    private List<OrderItem> orderItems = new ArrayList<>();
+
 }
