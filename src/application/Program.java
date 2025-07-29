aab0a22e93523c4872f2f3004df42719e32488c6
Joao Lucas <jldecristo@gmail.com>
2025-07-28 21:27:26 -0300

    instanciacao do order

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,11 @@
 package application;
 
+import entities.Client;
+import entities.Order;
+import entities.Product;
 import entities.enums.OrderStatus;
 
+import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.Locale;
 import java.util.Scanner;
@@ -20,9 +24,19 @@ public class Program {
         String name = sc.nextLine();
         System.out.print("Email: ");
         String email = sc.nextLine();
+        System.out.print("Birth date (DD/MM/YYYY): ");
+        LocalDate bithDate = LocalDate.parse(sc.nextLine(), fmt);
 
         System.out.println("Enter order data:");
         OrderStatus orderStatus = OrderStatus.valueOf(sc.nextLine());
+
+        Order order = new Order(LocalDate.now(), orderStatus, new Client(name, email, bithDate));
+
+        System.out.print("How many items to this order? ");
+        int n = sc.nextInt();
+        for (int i=0; i<n; i++) {
+            String productName = sc.nextLine();
+        }
 
 
 
